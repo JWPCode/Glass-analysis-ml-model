39536fd41e501da6b5ea2d84ee6a7422a5710bab
JWPCode <yonder06formats@icloud.com>
2025-01-10 14:21:44 +1000

    initial commit

---
+++ clasification.py
@@ -0,0 +1,858 @@
+[
+  {
+    "": 0,
+    "Type_of_glass": 1
+  },
+  {
+    "": 1,
+    "Type_of_glass": 1
+  },
+  {
+    "": 2,
+    "Type_of_glass": 1
+  },
+  {
+    "": 3,
+    "Type_of_glass": 1
+  },
+  {
+    "": 4,
+    "Type_of_glass": 1
+  },
+  {
+    "": 5,
+    "Type_of_glass": 1
+  },
+  {
+    "": 6,
+    "Type_of_glass": 1
+  },
+  {
+    "": 7,
+    "Type_of_glass": 1
+  },
+  {
+    "": 8,
+    "Type_of_glass": 1
+  },
+  {
+    "": 9,
+    "Type_of_glass": 1
+  },
+  {
+    "": 10,
+    "Type_of_glass": 1
+  },
+  {
+    "": 11,
+    "Type_of_glass": 1
+  },
+  {
+    "": 12,
+    "Type_of_glass": 1
+  },
+  {
+    "": 13,
+    "Type_of_glass": 1
+  },
+  {
+    "": 14,
+    "Type_of_glass": 1
+  },
+  {
+    "": 15,
+    "Type_of_glass": 1
+  },
+  {
+    "": 16,
+    "Type_of_glass": 1
+  },
+  {
+    "": 17,
+    "Type_of_glass": 1
+  },
+  {
+    "": 18,
+    "Type_of_glass": 1
+  },
+  {
+    "": 19,
+    "Type_of_glass": 1
+  },
+  {
+    "": 20,
+    "Type_of_glass": 1
+  },
+  {
+    "": 21,
+    "Type_of_glass": 1
+  },
+  {
+    "": 22,
+    "Type_of_glass": 1
+  },
+  {
+    "": 23,
+    "Type_of_glass": 1
+  },
+  {
+    "": 24,
+    "Type_of_glass": 1
+  },
+  {
+    "": 25,
+    "Type_of_glass": 1
+  },
+  {
+    "": 26,
+    "Type_of_glass": 1
+  },
+  {
+    "": 27,
+    "Type_of_glass": 1
+  },
+  {
+    "": 28,
+    "Type_of_glass": 1
+  },
+  {
+    "": 29,
+    "Type_of_glass": 1
+  },
+  {
+    "": 30,
+    "Type_of_glass": 1
+  },
+  {
+    "": 31,
+    "Type_of_glass": 1
+  },
+  {
+    "": 32,
+    "Type_of_glass": 1
+  },
+  {
+    "": 33,
+    "Type_of_glass": 1
+  },
+  {
+    "": 34,
+    "Type_of_glass": 1
+  },
+  {
+    "": 35,
+    "Type_of_glass": 1
+  },
+  {
+    "": 36,
+    "Type_of_glass": 1
+  },
+  {
+    "": 37,
+    "Type_of_glass": 1
+  },
+  {
+    "": 38,
+    "Type_of_glass": 1
+  },
+  {
+    "": 39,
+    "Type_of_glass": 1
+  },
+  {
+    "": 40,
+    "Type_of_glass": 1
+  },
+  {
+    "": 41,
+    "Type_of_glass": 1
+  },
+  {
+    "": 42,
+    "Type_of_glass": 1
+  },
+  {
+    "": 43,
+    "Type_of_glass": 1
+  },
+  {
+    "": 44,
+    "Type_of_glass": 1
+  },
+  {
+    "": 45,
+    "Type_of_glass": 1
+  },
+  {
+    "": 46,
+    "Type_of_glass": 1
+  },
+  {
+    "": 47,
+    "Type_of_glass": 1
+  },
+  {
+    "": 48,
+    "Type_of_glass": 1
+  },
+  {
+    "": 49,
+    "Type_of_glass": 1
+  },
+  {
+    "": 50,
+    "Type_of_glass": 1
+  },
+  {
+    "": 51,
+    "Type_of_glass": 1
+  },
+  {
+    "": 52,
+    "Type_of_glass": 1
+  },
+  {
+    "": 53,
+    "Type_of_glass": 1
+  },
+  {
+    "": 54,
+    "Type_of_glass": 1
+  },
+  {
+    "": 55,
+    "Type_of_glass": 1
+  },
+  {
+    "": 56,
+    "Type_of_glass": 1
+  },
+  {
+    "": 57,
+    "Type_of_glass": 1
+  },
+  {
+    "": 58,
+    "Type_of_glass": 1
+  },
+  {
+    "": 59,
+    "Type_of_glass": 1
+  },
+  {
+    "": 60,
+    "Type_of_glass": 1
+  },
+  {
+    "": 61,
+    "Type_of_glass": 1
+  },
+  {
+    "": 62,
+    "Type_of_glass": 1
+  },
+  {
+    "": 63,
+    "Type_of_glass": 1
+  },
+  {
+    "": 64,
+    "Type_of_glass": 1
+  },
+  {
+    "": 65,
+    "Type_of_glass": 1
+  },
+  {
+    "": 66,
+    "Type_of_glass": 1
+  },
+  {
+    "": 67,
+    "Type_of_glass": 1
+  },
+  {
+    "": 68,
+    "Type_of_glass": 1
+  },
+  {
+    "": 69,
+    "Type_of_glass": 1
+  },
+  {
+    "": 70,
+    "Type_of_glass": 2
+  },
+  {
+    "": 71,
+    "Type_of_glass": 2
+  },
+  {
+    "": 72,
+    "Type_of_glass": 2
+  },
+  {
+    "": 73,
+    "Type_of_glass": 2
+  },
+  {
+    "": 74,
+    "Type_of_glass": 2
+  },
+  {
+    "": 75,
+    "Type_of_glass": 2
+  },
+  {
+    "": 76,
+    "Type_of_glass": 2
+  },
+  {
+    "": 77,
+    "Type_of_glass": 2
+  },
+  {
+    "": 78,
+    "Type_of_glass": 2
+  },
+  {
+    "": 79,
+    "Type_of_glass": 2
+  },
+  {
+    "": 80,
+    "Type_of_glass": 2
+  },
+  {
+    "": 81,
+    "Type_of_glass": 2
+  },
+  {
+    "": 82,
+    "Type_of_glass": 2
+  },
+  {
+    "": 83,
+    "Type_of_glass": 2
+  },
+  {
+    "": 84,
+    "Type_of_glass": 2
+  },
+  {
+    "": 85,
+    "Type_of_glass": 2
+  },
+  {
+    "": 86,
+    "Type_of_glass": 2
+  },
+  {
+    "": 87,
+    "Type_of_glass": 2
+  },
+  {
+    "": 88,
+    "Type_of_glass": 2
+  },
+  {
+    "": 89,
+    "Type_of_glass": 2
+  },
+  {
+    "": 90,
+    "Type_of_glass": 2
+  },
+  {
+    "": 91,
+    "Type_of_glass": 2
+  },
+  {
+    "": 92,
+    "Type_of_glass": 2
+  },
+  {
+    "": 93,
+    "Type_of_glass": 2
+  },
+  {
+    "": 94,
+    "Type_of_glass": 2
+  },
+  {
+    "": 95,
+    "Type_of_glass": 2
+  },
+  {
+    "": 96,
+    "Type_of_glass": 2
+  },
+  {
+    "": 97,
+    "Type_of_glass": 2
+  },
+  {
+    "": 98,
+    "Type_of_glass": 2
+  },
+  {
+    "": 99,
+    "Type_of_glass": 2
+  },
+  {
+    "": 100,
+    "Type_of_glass": 2
+  },
+  {
+    "": 101,
+    "Type_of_glass": 2
+  },
+  {
+    "": 102,
+    "Type_of_glass": 2
+  },
+  {
+    "": 103,
+    "Type_of_glass": 2
+  },
+  {
+    "": 104,
+    "Type_of_glass": 2
+  },
+  {
+    "": 105,
+    "Type_of_glass": 2
+  },
+  {
+    "": 106,
+    "Type_of_glass": 2
+  },
+  {
+    "": 107,
+    "Type_of_glass": 2
+  },
+  {
+    "": 108,
+    "Type_of_glass": 2
+  },
+  {
+    "": 109,
+    "Type_of_glass": 2
+  },
+  {
+    "": 110,
+    "Type_of_glass": 2
+  },
+  {
+    "": 111,
+    "Type_of_glass": 2
+  },
+  {
+    "": 112,
+    "Type_of_glass": 2
+  },
+  {
+    "": 113,
+    "Type_of_glass": 2
+  },
+  {
+    "": 114,
+    "Type_of_glass": 2
+  },
+  {
+    "": 115,
+    "Type_of_glass": 2
+  },
+  {
+    "": 116,
+    "Type_of_glass": 2
+  },
+  {
+    "": 117,
+    "Type_of_glass": 2
+  },
+  {
+    "": 118,
+    "Type_of_glass": 2
+  },
+  {
+    "": 119,
+    "Type_of_glass": 2
+  },
+  {
+    "": 120,
+    "Type_of_glass": 2
+  },
+  {
+    "": 121,
+    "Type_of_glass": 2
+  },
+  {
+    "": 122,
+    "Type_of_glass": 2
+  },
+  {
+    "": 123,
+    "Type_of_glass": 2
+  },
+  {
+    "": 124,
+    "Type_of_glass": 2
+  },
+  {
+    "": 125,
+    "Type_of_glass": 2
+  },
+  {
+    "": 126,
+    "Type_of_glass": 2
+  },
+  {
+    "": 127,
+    "Type_of_glass": 2
+  },
+  {
+    "": 128,
+    "Type_of_glass": 2
+  },
+  {
+    "": 129,
+    "Type_of_glass": 2
+  },
+  {
+    "": 130,
+    "Type_of_glass": 2
+  },
+  {
+    "": 131,
+    "Type_of_glass": 2
+  },
+  {
+    "": 132,
+    "Type_of_glass": 2
+  },
+  {
+    "": 133,
+    "Type_of_glass": 2
+  },
+  {
+    "": 134,
+    "Type_of_glass": 2
+  },
+  {
+    "": 135,
+    "Type_of_glass": 2
+  },
+  {
+    "": 136,
+    "Type_of_glass": 2
+  },
+  {
+    "": 137,
+    "Type_of_glass": 2
+  },
+  {
+    "": 138,
+    "Type_of_glass": 2
+  },
+  {
+    "": 139,
+    "Type_of_glass": 2
+  },
+  {
+    "": 140,
+    "Type_of_glass": 2
+  },
+  {
+    "": 141,
+    "Type_of_glass": 2
+  },
+  {
+    "": 142,
+    "Type_of_glass": 2
+  },
+  {
+    "": 143,
+    "Type_of_glass": 2
+  },
+  {
+    "": 144,
+    "Type_of_glass": 2
+  },
+  {
+    "": 145,
+    "Type_of_glass": 2
+  },
+  {
+    "": 146,
+    "Type_of_glass": 3
+  },
+  {
+    "": 147,
+    "Type_of_glass": 3
+  },
+  {
+    "": 148,
+    "Type_of_glass": 3
+  },
+  {
+    "": 149,
+    "Type_of_glass": 3
+  },
+  {
+    "": 150,
+    "Type_of_glass": 3
+  },
+  {
+    "": 151,
+    "Type_of_glass": 3
+  },
+  {
+    "": 152,
+    "Type_of_glass": 3
+  },
+  {
+    "": 153,
+    "Type_of_glass": 3
+  },
+  {
+    "": 154,
+    "Type_of_glass": 3
+  },
+  {
+    "": 155,
+    "Type_of_glass": 3
+  },
+  {
+    "": 156,
+    "Type_of_glass": 3
+  },
+  {
+    "": 157,
+    "Type_of_glass": 3
+  },
+  {
+    "": 158,
+    "Type_of_glass": 3
+  },
+  {
+    "": 159,
+    "Type_of_glass": 3
+  },
+  {
+    "": 160,
+    "Type_of_glass": 3
+  },
+  {
+    "": 161,
+    "Type_of_glass": 3
+  },
+  {
+    "": 162,
+    "Type_of_glass": 3
+  },
+  {
+    "": 163,
+    "Type_of_glass": 5
+  },
+  {
+    "": 164,
+    "Type_of_glass": 5
+  },
+  {
+    "": 165,
+    "Type_of_glass": 5
+  },
+  {
+    "": 166,
+    "Type_of_glass": 5
+  },
+  {
+    "": 167,
+    "Type_of_glass": 5
+  },
+  {
+    "": 168,
+    "Type_of_glass": 5
+  },
+  {
+    "": 169,
+    "Type_of_glass": 5
+  },
+  {
+    "": 170,
+    "Type_of_glass": 5
+  },
+  {
+    "": 171,
+    "Type_of_glass": 5
+  },
+  {
+    "": 172,
+    "Type_of_glass": 5
+  },
+  {
+    "": 173,
+    "Type_of_glass": 5
+  },
+  {
+    "": 174,
+    "Type_of_glass": 5
+  },
+  {
+    "": 175,
+    "Type_of_glass": 5
+  },
+  {
+    "": 176,
+    "Type_of_glass": 6
+  },
+  {
+    "": 177,
+    "Type_of_glass": 6
+  },
+  {
+    "": 178,
+    "Type_of_glass": 6
+  },
+  {
+    "": 179,
+    "Type_of_glass": 6
+  },
+  {
+    "": 180,
+    "Type_of_glass": 6
+  },
+  {
+    "": 181,
+    "Type_of_glass": 6
+  },
+  {
+    "": 182,
+    "Type_of_glass": 6
+  },
+  {
+    "": 183,
+    "Type_of_glass": 6
+  },
+  {
+    "": 184,
+    "Type_of_glass": 6
+  },
+  {
+    "": 185,
+    "Type_of_glass": 7
+  },
+  {
+    "": 186,
+    "Type_of_glass": 7
+  },
+  {
+    "": 187,
+    "Type_of_glass": 7
+  },
+  {
+    "": 188,
+    "Type_of_glass": 7
+  },
+  {
+    "": 189,
+    "Type_of_glass": 7
+  },
+  {
+    "": 190,
+    "Type_of_glass": 7
+  },
+  {
+    "": 191,
+    "Type_of_glass": 7
+  },
+  {
+    "": 192,
+    "Type_of_glass": 7
+  },
+  {
+    "": 193,
+    "Type_of_glass": 7
+  },
+  {
+    "": 194,
+    "Type_of_glass": 7
+  },
+  {
+    "": 195,
+    "Type_of_glass": 7
+  },
+  {
+    "": 196,
+    "Type_of_glass": 7
+  },
+  {
+    "": 197,
+    "Type_of_glass": 7
+  },
+  {
+    "": 198,
+    "Type_of_glass": 7
+  },
+  {
+    "": 199,
+    "Type_of_glass": 7
+  },
+  {
+    "": 200,
+    "Type_of_glass": 7
+  },
+  {
+    "": 201,
+    "Type_of_glass": 7
+  },
+  {
+    "": 202,
+    "Type_of_glass": 7
+  },
+  {
+    "": 203,
+    "Type_of_glass": 7
+  },
+  {
+    "": 204,
+    "Type_of_glass": 7
+  },
+  {
+    "": 205,
+    "Type_of_glass": 7
+  },
+  {
+    "": 206,
+    "Type_of_glass": 7
+  },
+  {
+    "": 207,
+    "Type_of_glass": 7
+  },
+  {
+    "": 208,
+    "Type_of_glass": 7
+  },
+  {
+    "": 209,
+    "Type_of_glass": 7
+  },
+  {
+    "": 210,
+    "Type_of_glass": 7
+  },
+  {
+    "": 211,
+    "Type_of_glass": 7
+  },
+  {
+    "": 212,
+    "Type_of_glass": 7
+  },
+  {
+    "": 213,
+    "Type_of_glass": 7
+  }
+]
