18b684a034fdbc17314fc174890111d7c5a77f9b
JWPCode <yonder06formats@icloud.com>
2025-01-10 23:00:31 +1000

    added in graph to show nodes in the nural network, some code is there for the dendrites but is only the begining and needs to be worked on

--- glass analysis main.py
+++ glass analysis main.py
@@ -1,7 +1,8 @@
 import math
 import numpy as np
 import random
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import networkx as nx
 
 
 class Connection:
@@ -124,7 +125,167 @@ class Network:
         return output
 
     def output_network(self):
-      pass
+
+      node_x = []
+      node_y = []
+      counterx = 0
+      countery = 0
+      max_hight = 0
+      for layer in self.layers:
+          temp =len(layer)
+          if temp > max_hight:
+              max_hight = temp
+
+      for layer in self.layers:
+          countery = max_hight/2 - len(layer)/2
+          for node in layer:
+              node_x.append(counterx)
+              node_y.append(countery)
+              countery = countery +1
+          counterx = counterx +1
+
+      node_trace = go.Scatter(x=node_x, y=node_y, mode='markers',
+          hoverinfo='text',
+          marker=dict(
+              showscale=True,
+              # colorscale options
+              #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
+              #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
+              #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
+              colorscale='YlGnBu',
+              reversescale=True,
+              color=[],
+              size=10,
+              colorbar=dict(
+                  thickness=15,
+                  title=dict(
+                    text='Node Connections',
+                    side='right'
+                  ),
+                  xanchor='left',
+              ),
+              line_width=2))
+      
+
+      line_x = []
+      line_y = []
+      counterx = 0
+      countery = 0
+      #this is the issue here need to fix how the lines are drawn (x should be good, y needs fixing)
+      #requiers start and end coordinates of lines
+      for i,layer in enumerate(self.layers):
+          
+          for j, node in enumerate(layer):
+              countery = max_hight/2 - len(layer)/2
+
+              if i != len(self.layers)-1:
+                  for nnode in self.layers[i+1]:
+                      line_x.append(counterx)
+                      line_x.append(counterx+1)
+
+                  for nnode in self.layers[i+1]:
+                      line_y.append(countery)
+                      line_y.append(countery+1)
+                  
+
+              #line_y.append(countery)
+              countery = countery +1
+          counterx = counterx +1
+
+      line_trace = go.Scatter(
+          x=line_x, y=line_y,
+          line=dict(width=0.5, color='#888'),
+          hoverinfo='none',
+          mode='lines')
+      
+      figure = go.Figure(data=[node_trace, line_trace])
+
+      figure.show()
+        
+
+      """
+      G = nx.random_geometric_graph(200, 0.125)
+
+      edge_x = []
+      edge_y = []
+      for edge in G.edges():
+          x0, y0 = G.nodes[edge[0]]['pos']
+          x1, y1 = G.nodes[edge[1]]['pos']
+          edge_x.append(x0)
+          edge_x.append(x1)
+          edge_x.append(None)
+          edge_y.append(y0)
+          edge_y.append(y1)
+          edge_y.append(None)
+
+      edge_trace = go.Scatter(
+          x=edge_x, y=edge_y,
+          line=dict(width=0.5, color='#888'),
+          hoverinfo='none',
+          mode='lines')
+
+      node_x = []
+      node_y = []
+      for node in G.nodes():
+          x, y = G.nodes[node]['pos']
+          node_x.append(x)
+          node_y.append(y)
+
+      node_trace = go.Scatter(
+          x=node_x, y=node_y,
+          mode='markers',
+          hoverinfo='text',
+          marker=dict(
+              showscale=True,
+              # colorscale options
+              #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
+              #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
+              #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
+              colorscale='YlGnBu',
+              reversescale=True,
+              color=[],
+              size=10,
+              colorbar=dict(
+                  thickness=15,
+                  title=dict(
+                    text='Node Connections',
+                    side='right'
+                  ),
+                  xanchor='left',
+              ),
+              line_width=2))
+      
+      node_adjacencies = []
+      node_text = []
+      for node, adjacencies in enumerate(G.adjacency()):
+          node_adjacencies.append(len(adjacencies[1]))
+          node_text.append('# of connections: '+str(len(adjacencies[1])))
+
+      node_trace.marker.color = node_adjacencies
+      node_trace.text = node_text
+
+
+      fig = go.Figure(data=[edge_trace, node_trace],
+             layout=go.Layout(
+                title=dict(
+                    text="<br>Network graph made with Python",
+                    font=dict(
+                        size=16
+                    )
+                ),
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=20,l=5,r=5,t=40),
+                annotations=[ dict(
+                    text="Python code: <a href='https://plotly.com/python/network-graphs/'> https://plotly.com/python/network-graphs/</a>",
+                    showarrow=False,
+                    xref="paper", yref="paper",
+                    x=0.005, y=-0.002 ) ],
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                )
+      fig.show()
+      """
 
 #data stored in the worst way posible 214 values stored with ID's 0 - 213
 features = [
